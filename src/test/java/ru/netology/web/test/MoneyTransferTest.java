7f4e0048b59e73bf20c8c22adb36bcdb508c49a9
Konstantin Voytenko <koc.voytenko@gmail.com>
2022-03-08 20:50:02 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,41 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV3() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+}
+
